44441bec520c7b7c9b38f7d74383846df24ce364
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-15 04:05:28 +0300

    creating button component frontend

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,24 @@
+export default function Button({ text = "click", onClick, dark }) {
+  return (
+    <>
+      <button onClick={onClick}>{text}</button>
+      <style jsx="true">{`
+        button {
+          background: ${dark ? "white" : "#0072b1"};
+          color: ${dark ? "#0072b1" : "white"};
+          padding: 0.6rem 3rem;
+          border-radius: 30rem;
+          font-size: 1.2rem;
+          border: none;
+          cursor: pointer;
+          font-weight: 600;
+          width: 100%;
+          border: 1px solid #0072b1;
+        }
+        button:hover {
+          background: ${dark ? "#eeddff" : "#0092d1"};
+        }
+      `}</style>
+    </>
+  );
+}
