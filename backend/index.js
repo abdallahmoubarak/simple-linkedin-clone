ca5f15353861d340cc191f070ab67943f4dcebe9
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-15 19:56:27 +0300

    creating users and offers routes backend

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const authRoutes = require("./routes/auth.routes");
+const offerRoutes = require("./routes/offers.routes");
+const userRoutes = require("./routes/users.routes");
+
 app.use("/auth", authRoutes);
+app.use("/offers", offerRoutes);
+app.use("/users", userRoutes);
 
 app.listen(process.env.PORT, (err) => {
   if (err) console.log(err);
