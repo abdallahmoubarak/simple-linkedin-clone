4a13542bd82df4b7cc07797cb417475948c8b85c
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-15 21:09:06 +0300

    updating offers model backend

--- backend/models/offers.model.js
+++ backend/models/offers.model.js
@@ -7,6 +7,7 @@ const offerSchema = new mongoose.Schema({
   description: { type: String },
   requirments: { type: [String] },
   applicants: { type: [String] },
+  open: { type: Boolean, default: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
 });
