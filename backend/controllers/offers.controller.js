dcd3e13479d11082417f02bf00ee70ea8f406d28
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-15 21:00:36 +0300

    adding permission to creating offer backend

--- backend/controllers/offers.controller.js
+++ backend/controllers/offers.controller.js
@@ -2,14 +2,14 @@ const Offer = require("../models/offers.model");
 const User = require("../models/users.model");
 
 const createJobOffer = async (req, res) => {
-  const { company_id, title, description, requirments } = req.body;
+  const { title, description, requirments } = req.body;
 
-  if (!company_id || !title || !description || !requirments)
+  if (!title || !description || !requirments)
     return res
       .status(401)
       .json({ status: "error", massage: "values required" });
 
-  const company = await User.findOne(company_id).exec();
+  const company = await User.findById(req?.user?._id).exec();
   if (!company)
     return res
       .status(401)
@@ -17,7 +17,7 @@ const createJobOffer = async (req, res) => {
 
   try {
     const offer = new Offer();
-    offer.company_id = company_id;
+    offer.company_id = company.id;
     offer.company_name = company.name;
     offer.title = title;
     offer.description = description;
