a5f21ead75b8a8a55964a3e4f4bdcd6311c27626
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-15 19:41:02 +0300

    updating users model backend

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: "name is required",
-  },
+  name: { type: String, required: "name is required" },
   email: {
     type: String,
     required: "email is required",
@@ -16,6 +13,33 @@ const userSchema = new mongoose.Schema({
     required: "password is required",
     select: false,
   },
+  type: { type: String, required: "type is required" },
+  phone: { type: String },
+  birth: { type: Date },
+  profile_url: { type: String },
+  document_url: { type: String },
+  bio: { type: String },
+  skills: { type: [String] },
+  follows: { type: [String] },
+  educations: [
+    {
+      education: { type: String },
+      school: { type: String },
+      field_of_study: { type: String },
+      started_date: { type: String },
+      ended_date: { type: String },
+    },
+  ],
+  experiences: [
+    {
+      title: { type: String },
+      company: { type: String },
+      started_date: { type: String },
+      ended_date: { type: String },
+    },
+  ],
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
