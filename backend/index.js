05d3475754141d5aade04e4e853fa0f2347526f0
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-16 01:53:01 +0300

    adding cors backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 require("./config/db.config");
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const authRoutes = require("./routes/auth.routes");
 const offerRoutes = require("./routes/offers.routes");
