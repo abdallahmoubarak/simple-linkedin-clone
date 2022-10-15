9b41a78d38b57fb54fd2f975f76accbf7ca1eee6
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-15 19:40:42 +0300

    creating job offers model backend

---
+++ backend/models/offers.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const offerSchema = new mongoose.Schema({
+  company_id: { type: String },
+  title: { type: String },
+  description: { type: String },
+  requirments: { type: [String] },
+  applicants: { type: [String] },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+
+const Offer = mongoose.model("Offer", offerSchema);
+
+module.exports = Offer;
