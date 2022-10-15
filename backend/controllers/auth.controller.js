6d765fc8ae57ee7eec43f358dfc2b088f5bafb71
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-15 19:41:25 +0300

    updating sign up function backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,15 +3,28 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, type } = req.body;
+
+  if (!name || !email || !password || !type)
+    return res
+      .status(401)
+      .json({ status: "error", massage: "values required" });
+
   try {
     const user = new User();
     user.name = name;
     user.email = email;
     user.password = await bcrypt.hash(password, 10);
+    user.type = type;
 
+    const token = generateToken(user);
     await user.save();
-    res.json(user);
+
+    res.json({
+      status: "success",
+      user,
+      authorization: { token, type: "bearer" },
+    });
   } catch (err) {
     res.status(400).json({
       message: err.message,
@@ -21,7 +34,6 @@ const signup = async (req, res) => {
 
 const signin = async (req, res) => {
   const { email, password } = req.body;
-
   const user = await User.findOne({ email }).select("+password");
 
   if (!user) return res.status(404).json({ message: "Invalid Credentials" });
@@ -29,17 +41,13 @@ const signin = async (req, res) => {
   const isMatch = bcrypt.compare(password, user.password);
   if (!isMatch) return res.status(404).json({ message: "Invalid Credentials" });
 
-  const token = jwt.sign(
-    { email: user.email, name: user.name, userType: 1 },
-    process.env.JWT_SECRET_KEY,
-    {
-      expiresIn: "24h",
-    },
-  );
+  const token = generateToken(user);
   res.status(200).json(token);
 };
 
-module.exports = {
-  signin,
-  signup,
-};
+module.exports = { signin, signup };
+
+const generateToken = ({ email, name, type }) =>
+  jwt.sign({ email, name, type }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "24h",
+  });
