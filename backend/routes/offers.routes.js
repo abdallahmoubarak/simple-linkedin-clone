c21f91252d92e35a2d39c17c6b9df1f87577ae46
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-17 02:27:10 +0300

    creating apply offer route backend

--- backend/routes/offers.routes.js
+++ backend/routes/offers.routes.js
@@ -4,6 +4,7 @@ const {
   getJobOffers,
   getNotifications,
   getOwnOffers,
+  applyOffer,
 } = require("../controllers/offers.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 const companyMiddleware = require("../middleware/company.middleware");
@@ -11,6 +12,7 @@ const companyMiddleware = require("../middleware/company.middleware");
 const router = Router();
 
 router.post("/", companyMiddleware, createJobOffer);
+router.put("/", authMiddleware, applyOffer);
 router.get("/notifications", authMiddleware, getNotifications);
 router.get("/own", authMiddleware, getOwnOffers);
 router.get("/", authMiddleware, getJobOffers);
