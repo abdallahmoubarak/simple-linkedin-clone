17aa8702f30e1533cd9d3e0d142b4ec139084ea3
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-16 11:59:05 +0300

    adding search component frontend

--- frontend/src/components/Pages/PersonalPage.jsx
+++ frontend/src/components/Pages/PersonalPage.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { useFetchOffers } from "../../hooks/useOfferData";
 import ClientOfferCard from "../ClientOfferCard";
 
 export default function PersonalPage() {
   const { data: offers } = useFetchOffers();
+  const [search, setSearch] = useState("");
+
   return (
     <>
+      <div className="search-container">
+        <input
+          className="search"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <div className="cards-container">
         {offers?.map((offer, i) => (
           <ClientOfferCard
@@ -12,6 +23,7 @@ export default function PersonalPage() {
             title={offer.title}
             requirments={offer.requirments}
             companyName={offer.company_name}
+            imgUrl={offer.profile_img}
           />
         ))}
       </div>
