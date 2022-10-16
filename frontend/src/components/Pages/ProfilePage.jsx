dc27421e994894cf0a942cabf3903b3481a28c55
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-16 16:14:43 +0300

    editing profile page frontend

--- frontend/src/components/Pages/ProfilePage.jsx
+++ frontend/src/components/Pages/ProfilePage.jsx
@@ -1,10 +1,57 @@
+import { useState } from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import Skills from "../Skills";
+import Button from "../Button";
+import Input from "../Input";
+import UploadImage from "../UploadImage";
+import Education from "../Education";
+
 export default function ProfilePage({ setProfile }) {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [bio, setBio] = useState("");
+  const [skills, setSkills] = useState(["React"]);
+  const [educations, setEducations] = useState(defaultEducations);
+
   return (
     <>
-      <div className="pointer" onClick={() => setProfile(false)}>
-        back to offers
+      <div className="profile-container">
+        <div className="profile-personal-container">
+          <div
+            className="back-to-home pointer"
+            onClick={() => setProfile(false)}>
+            <IoIosArrowBack />
+            <span>Back to home</span>
+          </div>
+          <UploadImage />
+          <div className="inputs-container">
+            <Input name="Name" value={name} setValue={setName} />
+            <Input name="Phone" value={phone} setValue={setPhone} />
+            <textarea
+              placeholder="Bio"
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="profile-abilities-container">
+          <Skills skills={skills} setSkills={setSkills} />
+          <Education educations={educations} setEducations={setEducations} />
+
+          <div className="profile-btn-container">
+            <Button text="Save" />
+          </div>
+        </div>
       </div>
-      <div>Profile</div>
     </>
   );
 }
+
+const defaultEducations = [
+  {
+    school: "University of Isfahan",
+    degree: "Masters degree of Medical Information Systems",
+    started_date: "12-5-2017",
+    ended_date: "20-7-2020",
+  },
+];
