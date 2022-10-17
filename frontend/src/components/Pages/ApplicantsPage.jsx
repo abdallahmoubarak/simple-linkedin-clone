70798f6ce28c477d6c2af90f4aca9832341cebd9
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-17 15:37:04 +0300

    creating applicant page frontend

---
+++ frontend/src/components/Pages/ApplicantsPage.jsx
@@ -0,0 +1,41 @@
+import { IoIosArrowBack } from "react-icons/io";
+import { useApplicants } from "../../hooks/useUserData";
+import ApplicantCard from "../ApplicantCard";
+import Loader from "../Loader";
+import NoDataPlaceholder from "../NoDataPlaceholder";
+
+export default function ApplicantsPage({
+  setMain,
+  offerId,
+  handleOpenProfile,
+}) {
+  const { isLoading, data: applicants } = useApplicants(offerId);
+
+  return (
+    <>
+      <div className="applicant-page">
+        <div className="back-to-home pointer" onClick={() => setMain(true)}>
+          <IoIosArrowBack />
+          <span>Back to home</span>
+        </div>
+        {isLoading ? (
+          <Loader />
+        ) : !!applicants[0] ? (
+          <div className="cards-container">
+            {applicants
+              ?.map((applicant, i) => (
+                <ApplicantCard
+                  key={i}
+                  applicant={applicant}
+                  handleOpenProfile={handleOpenProfile}
+                />
+              ))
+              .reverse()}
+          </div>
+        ) : (
+          <NoDataPlaceholder name="applicants" />
+        )}
+      </div>
+    </>
+  );
+}
