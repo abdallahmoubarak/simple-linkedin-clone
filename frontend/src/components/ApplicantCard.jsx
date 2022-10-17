6f375cd54964be46ede6cbc3d116c853e1bcd3b8
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-17 15:37:33 +0300

    creating applicant card components frontend

---
+++ frontend/src/components/ApplicantCard.jsx
@@ -0,0 +1,30 @@
+import Button from "./Button";
+
+export default function ClientOfferCard({ applicant, handleOpenProfile }) {
+  return (
+    <div className="offer-card">
+      <div className="offer-card-head flex">
+        <div
+          className="flex pointer"
+          onClick={() => handleOpenProfile(applicant._id)}>
+          <div className="profile-img ">
+            <img
+              className="upload-img-img"
+              src={applicant.profile_url}
+              alt=""
+            />
+          </div>
+          <div className="offer-card-name ">{applicant.name}</div>
+        </div>
+        <div className="offer-card-name pointer">X</div>
+      </div>
+      <div className="offer-card-body">
+        <img className="pdf-img pointer" src="/img/pdf.svg" alt="" />
+
+        <div className="offer-card-btn-container">
+          <Button text={"Interview"} font="1rem" />
+        </div>
+      </div>
+    </div>
+  );
+}
